d3588f423406f273fd2ff8dc88bba5e318a59b10
nathanielatom <adam.subanloewen@gmail.com>
2013-05-17 00:54:37 -0400

    Update engine.py

--- engine.py
+++ engine.py
@@ -219,8 +219,8 @@ class Body(pygame.sprite.Sprite):
         # Stores acculated score
         self.points = 0.0
         # used to display info-bits when it surpasses the values in
-        self.point_lvls = list(point_lvls)
-
+        self.point_lvls = []
+        for point_lvl in point_lvls: self.point_lvls.append(point_lvl)
         self.visible = True
         self.particle = particle
         self.init_velocity = velocity
